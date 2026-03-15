3da063796271d12bf0364b772dfe2590483e2761
Leo Mulholland <leomulholland@Leos-MacBook-Pro.local>
2026-03-15 23:01:11 +0000

    new spectrum options, and cumulative contributions

--- spectrum.py
+++ spectrum.py
@@ -51,13 +51,29 @@ class spectrum:
 
 
     def totalSpectrum(self,listOfMasses):
-        self.masses = listOfMasses
+        self.masses = np.array(listOfMasses)
         ns = np.shape(self.spectra)[0]
         nw = np.shape(self.spectra)[1]
         self.total = np.zeros(nw)
+        self.integrals = np.zeros(ns)
+        self.cumulative = np.zeros_like(self.spectra)
+        
         for ii in range(0,ns):
-            self.total[:] += self.spectra[ii,:] * listOfMasses[ii]
+            contribution = self.spectra[ii,:] * listOfMasses[ii]
+            self.integrals[ii] += 0.1*np.trapz(contribution,self.wavelengths) #0.1 to convert units back to nm^-1
+
+        #self.order = np.argsort(self.integrals)[::-1]
+
+        for ii in range(0,ns):
+            contribution = self.spectra[ii,:] * listOfMasses[ii]
+            self.total[:] += contribution
+            self.cumulative[ii,:] = contribution 
+            #Calculate cumulatives - this could probably be made more efficient with Numpy 
+            for kk in range(0,ii):
+                self.cumulative[ii,:] += self.spectra[kk,:] * listOfMasses[kk]
+                
         self.total += self.continuum
+        self.sumIntegrals = np.sum(self.integrals)
         
     def blackBodyAndAfterglow(self, bbT,agA,agB):
         wavelength_range = self.wavelengths
@@ -79,13 +95,24 @@ class spectrum:
         self.continuum = continuum
         #return continuum 
         
-    def plot(self,dpi = 200,xlimits = [500,5000],ylimits = [0,1.2],colormap = 'rainbow'):
+    def plot(self,
+             dpi = 200,
+             xlimits = [500,5000],
+             ylimits = [0,1.2],
+             colormap = 'tab20',
+             legend=True,
+             cumulative=False,
+             integralThresh = 0.01,
+             massThresh = 1e-10,
+             printInterestingLines=False,
+             interestingThresh = 1e37):
         import matplotlib.pyplot as plt 
         from matplotlib.ticker import FormatStrFormatter
         from matplotlib import rc
+        
         rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
         plt.rcParams['text.usetex'] = True
-        plt.rcParams.update({'font.size': 14})
+        plt.rcParams.update({'font.size': 12})
 
         cmap = plt.get_cmap(colormap)
         colors = [cmap(i) for i in np.linspace(0, 1, self.numSpec+1)]
@@ -95,42 +122,80 @@ class spectrum:
         vfiwl,vfilum  = getVFI()
         
         plt.step(vfiwl,vfilum * 1e-35,'k',linewidth=0.2)
-        
-        
-        for ii in range(0,self.numSpec):
-            
-            # FOrmatting scientific notation in matplotlib with latex is voodoo.
+
+        if (cumulative): 
+            loopRange = range(self.numSpec-1,-1,-1)
+            #
+        else:
+            loopRange = np.argsort(self.integrals)[::-1]
+            #
+        #
+        #loopRange = range(self.numSpec-1,-1,-1)
+
+        #loopRange = np.argsort(self.integrals)[::-1]
+        #print(loopRange)
+        for ii in loopRange:
+            #
+            if cumulative:
+                #
+                ax.fill_between(self.wavelengths,self.cumulative[ii,:]*1e-35,color = colors[ii+1])
+            else:
+                #
+                thisSpec = self.spectra[ii,:] * self.masses[ii] * 1e-35
+                ax.fill_between(self.wavelengths,thisSpec,color = colors[ii+1])
+                #ax.plot(self.wavelengths,thisSpec,color = colors[ii+1])
+
+            #
+            # Formatting scientific notation in matplotlib with latex is voodoo.
             massStringVoodoo = '{:8.2e}'.format(self.masses[ii])
-            exphack = int(massStringVoodoo[-1])
-#
-            if (self.masses[ii] <= 1e-10):
-                #exponent requires two digits...
-                import sys 
-                sys.exit()
-                print('masses wont be formatted properly - debug @ leomulholland@hotmail.com')
-#            #as such, the following three lines were greatly aided by Gemini.
-#
+            exphack = int(massStringVoodoo[-1])    
             exponent = f"-{exphack}"
             massLabel = fr",~M = {massStringVoodoo[:4]} $\times 10^{{{exponent}}} \mathrm{{ M}}_\odot$"
             
-            thisSpec = self.spectra[ii,:] * self.masses[ii] * 1e-35
+            #print('{:8} {:8.2e} {:8.2e}'.format(self.elements[ii],self.integrals[ii],self.integrals[ii]/self.sumIntegrals))
 
-            ax.fill_between(self.wavelengths,thisSpec,color = colors[ii+1])
-            #print(massMantessa,exphack,massLabel1.format(massMantessa))
-            print(massLabel)
-            #massLabel = massLabel1.format(massMantessa) + massLabel2 + massLabel3.format(exphack) + massLabel4
-            
+            #This plot is just for the legend.
             plt.plot(-1,-1,
                      color = colors[ii+1],
-                     label = self.elements[ii] + massLabel )
+                     label = '{:>6}'.format(self.elements[ii]) + massLabel )
+            
+        if printInterestingLines:
+            for ii in range(0,self.numSpec):
+                if (self.masses[ii] > massThresh):
+                    lumo = self.colRadLumo_classes[ii].luminosity_ergs_per_solar_mass[:]
+                    wll   = self.colRadLumo_classes[ii].wl_vac_nm
+                    #print(wll)
+                    mass = self.masses[ii]
+                    ll = lumo [lumo > interestingThresh/mass]
+                    wl = wll [lumo[:,0,0] > interestingThresh/self.masses[ii]]
+                    if len(ll) > 0:
+                        for kk in range(0,len(wl)):
+                            print('{:>8} {:8.2f} {:8.2e}'.format(self.elements[ii],wl[kk],ll[kk]))
         
+        #Plot total
         plt.plot(self.wavelengths,self.total* 1e-35,color = colors[0])
         
+        #Formatting, and so forth.
         plt.xlim(xlimits)
         plt.ylim(ylimits)
         plt.xlabel('Wavelength (nm)')
         plt.ylabel(r'Luminosity (10$^3$$^5$ erg s$^{-1}$ Å$^{-1}$)')
-        plt.legend(loc = 'upper left')
+        
+        if legend:
+            #
+            #various voodoo's to get the legend in the order of the input files.
+            legendOrder = np.argsort(loopRange)
+            handles, labels = plt.gca().get_legend_handles_labels()
+            handles = [handles[i] for i in legendOrder]
+            labels = [labels[i] for i in legendOrder]
+            
+            mask = (self.masses > massThresh) & (self.integrals > integralThresh * self.sumIntegrals)
+            handles = np.array(handles, dtype=object)[mask]
+            labels  = np.array(labels,  dtype=object)[mask]
+            ax.legend(handles,labels,loc="center left", bbox_to_anchor=(1, 0.5))
+            #plt.legend(handles,labels,loc = 'upper left')
+            #
+        #
         return fig,ax
 
 
